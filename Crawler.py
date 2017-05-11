06804df7bf0c3e74f022be179cb6500d58b3761c
yolux.wang <yolux.wang@beseye.com>
2017-05-11 22:59:16 +0800

    add lesson-one

--- Crawler.py
+++ Crawler.py
@@ -14,9 +14,9 @@ class Crawler():
     def get_exchange_rate_table(cls, currency_table):
         '''
         input:
-            (USD','SGD','SEK','ZAR','HKD','CHF','EUR','CNY','GBP','CAD','NZD','AUD','JPY','THB')
+            (USD','JPY')
         output:
-            {'SGD': '21.4240', 'THB': '0.8694', 'CHF': '29.9822', 'NZD': '20.6702', 'HKD': '3.8854', 'GBP': '39.1246', 'ZAR': '2.2403', 'SEK': '3.3882', 'JPY': '0.2648', 'USD': '30.2550', 'EUR': '32.8758', 'CNY': '4.3825', 'CAD': '22.0670', 'AUD': '22.2553'}
+            {'JPY': '0.2648', 'USD': '30.2550'}
         '''
         pass
 
@@ -26,22 +26,40 @@ class ITERExchangeRateCrawler(Crawler):
         ret = {}
         r = requests.get('https://tw.rter.info/capi.php')
         j = r.json()
-        for currency in currency_table:
-            if currency == 'USD':
-                ret['USD'] = round(j['USDTWD']['Exrate'], 4)
-            else:
-                ret[currency] = round(j['USDTWD']['Exrate'] / j['USD'+currency]['Exrate'], 4)
+
+        # HERE! ->
+        #
+        # 預期的 input 參數 currency_table 為 (USD','JPY')
+        #
+        # 我們希望使用 問回來的 資料把它轉換成一個 dictionary 像這樣
+        #
+        # {'JPY': '0.2648', 'USD': '30.2550'}
+        #
+        #
+        pass
+        #
+        # HERE! <-
+
         return ret
 
 class GoogleExchangeRateCrawler(Crawler):
     @classmethod
     def get_exchange_rate_table(cls, currency_table):
         ret = {}
-        for currency in currency_table:
-            # find the pattern like "<div id=currency_converter_result>1 USD = <span class=bld>30.2550 TWD</span>"
-            r = requests.get("https://www.google.com/finance/converter?a=1&from=%s&to=%s" % (currency, 'TWD'))
-            match_obj = re.search("<span class=bld>(\d+\.?\d*) [A-Z]{3}<\/span>", r.text)
-            if match_obj:
-                ret[currency] = match_obj.group(1)
-            time.sleep(0.2)
+        
+        # HERE! ->
+        #
+        # 預期的 input 參數 currency_table 為 (USD','JPY')
+        #
+        # 我們希望使用 問回來的 資料把它轉換成一個 dictionary 像這樣
+        #
+        # {'JPY': '0.2648', 'USD': '30.2550'}
+        #
+        # 你可以用下面方式抓取整個網頁回來研究看資料在哪邊
+        #
+        # r = requests.get("https://www.google.com/finance/converter?a=1&from=%s&to=%s" % (currency, 'TWD'))
+        #
+        pass
+        #
+        # HERE! <-
         return ret
--- MyLineBot.py
+++ MyLineBot.py
@@ -8,67 +8,20 @@ from datetime import datetime
 
 class MyLineBot:
     # 支援的幣別
-    support_currency = ('USD','SGD','SEK','ZAR','HKD','CHF','EUR','CNY','GBP','CAD','NZD','AUD','JPY','THB')
+    support_currency = ('USD','JPY')
     # 幣別名稱
-    currency_name_mapping = {'USD':'美元','SGD':'新加坡幣','SEK':'瑞典克郎','ZAR':'南非幣','HKD':'港幣','CHF':'瑞士法郎','EUR':'歐元','CNY':'人民幣 ','GBP':'英磅','CAD':'加幣','NZD':'紐西蘭幣','AUD':'澳幣','JPY':'日圓','THB':'泰銖'}
+    currency_name_mapping = {'USD':'美元','JPY':'日圓'}
     inverse_currency_name_mapping = {v: k for k, v in currency_name_mapping.items()}
     # 使用哪些爬蟲
     crawlers = (ITERExchangeRateCrawler, GoogleExchangeRateCrawler)
     # 暫存資料
-    # exchange_rate_table 格式如下： {'SGD': '21.4240', 'THB': '0.8694', 'CHF': '29.9822', 'NZD': '20.6702', 'HKD': '3.8854', 'GBP': '39.1246', 'ZAR': '2.2403', 'SEK': '3.3882', 'JPY': '0.2648', 'USD': '30.2550', 'EUR': '32.8758', 'CNY': '4.3825', 'CAD': '22.0670', 'AUD': '22.2553'}
+    # exchange_rate_table 格式如下： {'JPY': '0.2648', 'USD': '30.2550'}
     exchange_rate_table = {}
-    # notify_list 格式自訂
     notify_list = {}
     table_update_timestamp = str(datetime.utcnow())+'(UTC)'
 
     def __init__(self, options={}):
-        # **
-        self.push_callback = options.get('push_callback', None)
-        self.exchange_rate_table = options.get('exchange_rate_table', {})
-
-    # ---- ** 通知使用者 ----
-    def notify_user(self, uid, msg):
-        print("notify_user '%s' '%s'" % (uid, msg))
-        if self.push_callback:
-            self.push_callback([uid], msg)
-
-    # ---- ** 新增通知名單 ----
-    def add_request_to_notify_list(self, uid, currency, updown, value):
-        # HERE! ->
-        #
-        # 當一位使用者 "ab123" 的通知需求為 '當美元大於35.0元的時候請記得通知該使用者''
-        # 我們預期會得到以下的參數：
-        #
-        # (uid, currency, updown, value) -> (ab123, 美元, 大於, 35.0)
-        # 
-        # 請利用 self.notify_list 將參數記錄下來
-        #
-        # 另外，我們要如何避免 使用者重複記錄同一種幣別通知?
-        #
-        #     例如 
-        #     add_request_to_notify_list( ab123, 美元, 大於, 35.0)
-        #     add_request_to_notify_list( ab123, 美元, 大於, 25.0)
-        #
-        # 記錄了兩次 USD 的通知請求，我們該如何只保留最後一筆資料?
-        #
         pass
-        #
-        # HERE! <-
-
-
-    # ---- ** 檢查通知名單 ----
-    def check_notify_list(self):
-        # HERE! ->
-        #
-        # 當我們取得最新匯率表的時候，按照 self.notify_list 裡面的參數 決定是否要通知使用者
-        #
-        # 請使用 self.notify_user() 將訊息傳遞給使用者，例如
-        #
-        #     self.notify_user("ab123", "你好，......")
-        #
-        pass
-        #
-        # HERE! <-
 
     # ---- 取得通知名單 ----
     def get_notify_list(self):
@@ -83,9 +36,6 @@ class MyLineBot:
                 break
         self.exchange_rate_table = table
         self.table_update_timestamp = str(datetime.utcnow())+'(UTC)'
-
-        # ** 
-        self.check_notify_list()
 
     # ---- 取得匯率表 ----
     def get_exchange_rate_table(self):
@@ -108,46 +58,31 @@ class MyLineBot:
     def parse_message(self, uid, msg):
         # --- 預設選單 ---
         msg = msg.strip().replace(' ','')
-        default_msg = "請輸入你想查詢的幣別（目前支援的有 %s）\n或請輸入你想被通知的幣別（範例1： 美元大於30通知我, 範例2： 日圓小於0.27通知我）" % (",".join(self.currency_name_mapping.values()))
+        default_msg = "請輸入你想查詢的幣別（目前支援的有 %s）" % (",".join(self.currency_name_mapping.values()))
 
-        # --- ** 通知功能 ----
-        if re.match(".*通知", msg):  
-            # HERE! ->
-            #
-            # 將使用者的訊息 拆解成三個字串參數出來 ， 分別對應到 （currency, updown, value）
-            #
-            #    例如 "美元大於30通知我" -> (美元, 大於, 30)
-            #
-            # 也有可能發生使用者的參數拆解出來無意義的時候：
-            #
-            #    例如 "黃金大於30通知我" -> (黃金, 大於, 30)
-            #
-            #    例如 "美元30通知我" -> (美元, ?, 30)
-            #
-            # 試著想想如何處理以上這些問題
-            #
-            currency = ""
-            updown = ""
-            value = ""
-            #
-            # HERE! <-
-            if len(currency) > 0 and len(updown) > 0 and len(value) > 0:
-                self.add_request_to_notify_list(uid, currency, updown, value)
-                return "已確認，%s%s%s通知你" % (currency, updown, value)
-            else:
-                return "請檢查你的格式（範例1： 美元大於30通知我, 範例2： 日圓小於0.27通知我）"
-
-        # --- 匯率查詢功能 ----
-        for currency, name in self.currency_name_mapping.items():
-            if re.match(name, msg):
-                return "%s匯率現在是 %.4f" % (name, self.get_exchange_rate(currency))
+        # --- ** 匯率查詢功能 ----
+        # HERE! ->
+        #
+        # 如何知道使用者想查詢的是哪個幣別?
+        #
+        # 例如  使用者的訊息為 "請問現在美元匯率"
+        #
+        # 我們要能知道他問的是 "美元"
+        #
+        # 再來，我們要利用 self.get_exchange_rate 去查表 回傳匯率
+        #
+        # 你或許會用到 self.currency_name_mapping 幣別名稱和代碼的轉換表
+        #
+        pass
+        #
+        # HERE! <-
 
         return default_msg
 
 
 if __name__ == "__main__":
 
-    bot = MyLineBot({'exchange_rate_table':{'USD':35.5, 'JPY':0.29}})
+    bot = MyLineBot()
 
     def msg_test(title, uid, msg, is_fail_pattern):
         print(title)
@@ -165,38 +100,21 @@ if __name__ == "__main__":
                 print("\n[0.驗證初始狀態]")
                 print(bot.get_table_update_timestamp(), bot.get_exchange_rate_table(), bot.get_notify_list())
 
-                # print("\n[0.驗證爬蟲] test_crawlers()")
-                # bot.test_crawlers()
+                print("\n[0.驗證爬蟲] test_crawlers()")
+                bot.test_crawlers()
 
-                if msg_test("\n[1.驗證訊息 'hi' ] parse_message('hi')", "","hi", False):
-                    break
+                msg_test("\n[1.驗證訊息 'hi' ] parse_message('hi')", "","hi", False)
 
-                if msg_test("\n[2.驗證訊息 '美 元' ] parse_message('美 元')]", "","美 元", True):
-                    break
+                msg_test("\n[2.驗證訊息 '美 元' ] parse_message('美 元')]", "","美 元", True)
 
-                if msg_test("\n[3.驗證訊息 '美金' ] parse_message('美金')]", "","美金", False):
-                    break
+                msg_test("\n[3.驗證訊息 '美金' ] parse_message('美金')]", "","美金", False)
 
-                msg_test("\n[4.驗證訊息 '黃金大於30通知我' ] parse_message('黃金大於30通知我')]", "abcd1234", "黃金大於30通知我", False)
-                bot.check_notify_list()
-                bot.get_notify_list()
+                print("\n[4.驗證更新匯率表] update_exchange_rate_table()")
+                bot.update_exchange_rate_table()
+                print(bot.get_table_update_timestamp(), bot.get_exchange_rate_table(), bot.get_notify_list())
 
-                msg_test("\n[5.驗證訊息 '如果美元大於 30 通知我' ] parse_message('如果美元大於 30 通知我')]", "abcd1234", "如果美元大於 30 通知我", False)
-                bot.get_notify_list()
-                bot.check_notify_list()
-
-                msg_test("\n[7.驗證訊息 '美元大於 30塊 通知我' ] parse_message('美元大於 30塊 通知我')]", "abcd1234", "美元大於 30塊 通知我", True)
-                bot.get_notify_list()
-                bot.check_notify_list()
+                msg_test("\n[5.驗證訊息 '美元' ] parse_message('美元')]", "","美元", True)
                 
-                msg_test("\n[8.驗證訊息 '美元大於40通知我' ] parse_message('美元大於40通知我')]", "abcd1234", "美元大於40通知我", True)
-                bot.get_notify_list()
-                bot.check_notify_list()
-
-                msg_test("\n[9.驗證訊息 '美元大於 30 通知我' ] parse_message('美元大於 30 通知我')]", "abcd1234", "美元大於 30 通知我", True)
-                bot.get_notify_list()
-                bot.check_notify_list()
-
                 break
 
             
